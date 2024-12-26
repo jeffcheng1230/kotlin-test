558411a7e94772a46dcf107c99b16338d92c4953
Jeffrey Cheng <jeffreycheng1230@gmail.com>
2024-12-25 22:03:53 -0500

    working activity_main.xml

--- app/src/main/java/com/example/kotlin_test/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_test/MainActivity.kt
@@ -15,17 +15,18 @@ import com.example.kotlin_test.ui.theme.KotlintestTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            KotlintestTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
+//        setContent {
+//            KotlintestTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    Greeting("Android")
+//                }
+//            }
+//        }
     }
 }
 
